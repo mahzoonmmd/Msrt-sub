5f07c8478a55ee2efa05eef2baec6312f028f084
Mmd Mahzoon <56454960+mahzoonmmd@users.noreply.github.com>
2026-07-14 00:30:51 +0330

    Add ffprobe to build script for long video support

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 pip install pyinstaller
-pyinstaller --noconfirm --onefile --windowed --name "MSRT" --icon "icon.ico" --add-binary "icon.ico;." --add-binary "icon_256.png;." --add-binary "C:\ffmpeg-8.1.2-essentials_build\bin\ffmpeg.exe;." --add-binary "C:\ffmpeg-8.1.2-essentials_build\bin\ffprobe.exe;." --hidden-import "groq" --hidden-import "PyQt6" app.py
+pyinstaller --noconfirm --onefile --windowed --name "MSRT" --icon "icon.ico" --add-binary "ffmpeg.exe;." --add-binary "ffprobe.exe;." --add-binary "icon.ico;." --add-binary "icon_256.png;." --hidden-import "groq" --hidden-import "PyQt6" app.py
 echo Done! MSRT.exe is in dist folder.
 pause
